c99d98f35fffd889ced24bc22db23d0ccaab36ac
joacrofe <joacrofe@gmail.com>
2022-09-05 14:52:21 -0300

    Agregar archivos de proyecto.

---
+++ Diccionario/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Diccionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sistema s = new Sistema();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Diccionario/Sistema.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Diccionario
+{
+    public class Sistema
+    {
+        List<Palabra> palabras = new List<Palabra>();
+        public Sistema()
+        {
+            Precarga();
+        }
+
+        private void Precarga()
+        {
+
+           string path =  AppContext.BaseDirectory+"/diccionario_espanol.txt";
+           
+            using (StreamReader file = new StreamReader(path))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                  string[] parts = ln.Split('#');
+                    palabras.Add(new Palabra(parts[0],parts[1]));
+                }
+                file.Close();
+            }
+        }
+    }
+}
