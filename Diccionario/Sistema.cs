9307edddb4eedd21a2c6e5cd59a19bca46584e47
joacrofe <joacrofe@gmail.com>
2022-09-06 12:01:51 -0300

    update

--- Diccionario/Sistema.cs
+++ Diccionario/Sistema.cs
@@ -17,16 +17,26 @@ namespace Diccionario
 
         private void Precarga()
         {
+            //1 - Obtenemos el archivo txt que contiene todas las palabras con su significado.
             string workingDirectory = Environment.CurrentDirectory;
             string path = Directory.GetParent(workingDirectory).Parent.Parent.FullName+"/diccionario_espanol.txt";
            
+            
             using (StreamReader file = new StreamReader(path))
             {
-                int counter = 0;
+               
                 string ln;
+                //2 - Recorremos linea a linea mientras existan.
                 while ((ln = file.ReadLine()) != null)
                 {
-                  string[] parts = ln.Split('#');
+                    /*
+                     Creamos un array pequeño donde en la posición
+                    cero se guarda el nombre de la palabra, y en la
+                    posición uno se guarda su significado.
+                    Lo utilizamos para crear una instancia de palabra
+                    y agregarla a la lista.
+                     */
+                    string[] parts = ln.Split('#');// https://docs.microsoft.com/en-us/dotnet/api/system.string.split?view=net-6.0
                     palabras.Add(new Palabra(parts[0],parts[1]));
                 }
                 file.Close();
